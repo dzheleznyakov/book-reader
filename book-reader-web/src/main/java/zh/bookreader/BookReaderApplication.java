fec0ff3b62eb807b3383e959f4eff20ab29a040f
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2020-08-31 13:25:16 +0100

    Set up basic project structure. #1

---
+++ book-reader-web/src/main/java/zh/bookreader/BookReaderApplication.java
@@ -0,0 +1,11 @@
+package zh.bookreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookReaderApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BookReaderApplication.class, args);
+	}
+}
