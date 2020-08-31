3e2676714723c6976cb2846b2da90c3aac579df7
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2020-08-31 23:08:33 +0100

    Include React.js UI module into the Maven build. Closes #2

--- book-reader-web/src/test/java/zh/bookreader/config/WebApplicationConfigTest.java
+++ book-reader-web/src/test/java/zh/bookreader/config/WebApplicationConfigTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @WebMvcTest
 class WebApplicationConfigTest {
@@ -11,6 +12,10 @@ class WebApplicationConfigTest {
     MockMvc mockMvc;
 
     @Test
-    void name() {
+    void name() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andDo(result -> {
+                    System.out.println("result = " + result);
+                });
     }
 }
