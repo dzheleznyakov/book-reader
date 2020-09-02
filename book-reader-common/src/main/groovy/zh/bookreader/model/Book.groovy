898006a5ab76c77803b195bf82fc8d4fb8ee8235
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2020-09-02 14:15:59 +0100

    Design BookService interface. Closes #8

--- book-reader-common/src/main/groovy/zh/bookreader/model/Book.groovy
+++ book-reader-common/src/main/groovy/zh/bookreader/model/Book.groovy
@@ -1,6 +1,7 @@
 package zh.bookreader.model
 
 class Book {
+    String id
     Cover cover
     List<Chapter> chapters
 }
---
+++ book-reader-common/src/main/java/zh/bookreader/services/BookService.java
@@ -0,0 +1,24 @@
+package zh.bookreader.services;
+
+import zh.bookreader.model.Book;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.Optional;
+
+public interface BookService {
+    @Nonnull
+    List<Book> findAll();
+
+    @Nonnull
+    Optional<Book> findById();
+
+    @Nonnull
+    List<Book> findByTitle(String title);
+
+    @Nonnull
+    List<Book> findByAuthor(String author);
+
+    @Nonnull
+    List<Book> findByTopic(String topic);
+}
