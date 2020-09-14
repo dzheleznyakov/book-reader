2b35388f24bcd5658669baad226e52bbf45abc33
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2020-09-14 20:18:27 +0100

    UI: add Image.test.js. #19

---
+++ book-reader-react-ui/src/main/app/src/__test__/components/BookList/Image/Image.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import Image from '../../../../components/BookList/Image/Image';
+import { expect } from 'chai';
+
+describe("<Image />", () => {
+    const props = { image: [0, 1, 2, 3] };
+    let wrapper;
+    let realCreateObjectURL;
+    let mockCreateObjectURL
+
+    beforeAll(() => {
+        realCreateObjectURL = global.URL.createObjectURL;
+    });
+
+    beforeEach(() => {
+        mockCreateObjectURL = sinon.mock();
+        global.URL.createObjectURL = mockCreateObjectURL;        
+    });
+
+    afterAll(() => {
+        global.URL.createObjectURL = realCreateObjectURL;
+    });
+
+    const getImageSrc = () => wrapper.find('img').prop('src');
+
+    test("render without errors", () => {
+        const mockUrl = 'https://mock-url.net';
+        mockCreateObjectURL
+            .returns(mockUrl);
+        
+        wrapper = mount(<Image {...props} />);
+
+        expect(getImageSrc()).to.be.equal(mockUrl);
+    });
+});
