6a0e5cd60ce43dde2845e986f66b5616c139fa3b
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2020-09-21 13:51:39 +0100

    Add unit tests for handling hrefs. #25

--- book-reader-restful-api/src/test/java/zh/bookreader/api/converters/EnclosingDocumentToEnclosingDocumentCommandConverterTest.java
+++ book-reader-restful-api/src/test/java/zh/bookreader/api/converters/EnclosingDocumentToEnclosingDocumentCommandConverterTest.java
@@ -1,19 +1,28 @@
 package zh.bookreader.api.converters;
 
+import com.google.common.collect.ImmutableMap;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import zh.bookreader.api.commands.EnclosingDocumentCommand;
+import zh.bookreader.model.DocumentType;
+import zh.bookreader.model.EnclosingDocument;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 import static zh.bookreader.api.ApiTestUtils.PAR;
 import static zh.bookreader.api.ApiTestUtils.SEC_1;
 import static zh.bookreader.api.ApiTestUtils.SEC_2;
+import static zh.bookreader.api.ApiTestUtils.TEXT_DOC_1;
 import static zh.bookreader.api.testtuils.hamcrest.DocumentMatchers.stemsFrom;
 
 class EnclosingDocumentToEnclosingDocumentCommandConverterTest {
+    private static final String METADATA_HREF_KEY = "@href";
+    private static final String METADATA_DATA_TYPE_KEY = "type";
+    private static final String METADATA_XREF_DATA_TYPE = "xref";
+
     private EnclosingDocumentToEnclosingDocumentCommandConverter converter;
 
     @BeforeEach
@@ -52,5 +61,35 @@ class EnclosingDocumentToEnclosingDocumentCommandConverterTest {
         EnclosingDocumentCommand command = converter.convert(SEC_2);
 
         assertThat(command, stemsFrom(SEC_2));
+    }
+
+    @Test
+    @DisplayName("Test converting an HREF document pointing to an external page")
+    void testHrefConversion_External() {
+        String href = "http://mock-link";
+        EnclosingDocument hrefDoc = EnclosingDocument.builder(DocumentType.HREF)
+                .withContent(TEXT_DOC_1)
+                .withMetadata(ImmutableMap.of(METADATA_HREF_KEY, href))
+                .build();
+
+        EnclosingDocumentCommand command = converter.convert(hrefDoc);
+
+        assertThat(command.getHref(), is(equalTo(href)));
+    }
+
+    @Test
+    @DisplayName("Test converting an HREF document pointing to an internal page")
+    void testHrefConversion_Internal() {
+        String href = "mock-link.html#pos";
+        EnclosingDocument hrefDoc = EnclosingDocument.builder(DocumentType.HREF)
+                .withContent(TEXT_DOC_1)
+                .withMetadata(ImmutableMap.of(
+                        METADATA_HREF_KEY, href,
+                        METADATA_DATA_TYPE_KEY, METADATA_XREF_DATA_TYPE))
+                .build();
+
+        EnclosingDocumentCommand command = converter.convert(hrefDoc);
+
+        assertThat(command.getHref(), is(equalTo("mock-link#pos")));
     }
 }
