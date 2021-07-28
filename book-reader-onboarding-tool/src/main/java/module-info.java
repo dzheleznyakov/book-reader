5794a7f4f972ce92add55d7d3c87f2d8a60d2c9e
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2021-07-28 01:47:32 +0100

    Re-set up OnboardingApp module. #72

--- book-reader-onboarding-tool/src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module book.reader.onboarding.tool {
-    requires javafx.graphics;
-    requires javafx.controls;
-
-    opens zh.op.bookreader.onboarding;
-}
--- book-reader-onboarding-tool/src/main/java/zh/op/bookreader/onboarding/OnboardingApp.java
+++ book-reader-onboarding-tool/src/main/java/zh/op/bookreader/onboarding/OnboardingApp.java
@@ -1,26 +1,11 @@
 package zh.op.bookreader.onboarding;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-public class OnboardingApp extends Application {
-    @Override
-    public void start(Stage stage) {
-        VBox root = new VBox();
-        Label label = new Label();
-        Button button = new Button();
-
-        root.getChildren().addAll(label, button);
-
-        button.setOnMouseClicked(me -> label.setText("Hello World"));
-
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.setTitle("Test");
-        stage.show();
+@SpringBootApplication
+public class OnboardingApp {
+    public static void main(String[] args) {
+        SpringApplication.run(OnboardingApp.class, args);
     }
 }
